e1cf91f7f5c883000778ac16aead7dae7ceacc83
samuelp244 <samuelp244@gmail.com>
2022-08-18 13:28:16 +0530

    backend for login and register

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import LogIn from './components/forms/CustomerForms/LogInComm';
 import SignUp from './components/forms/CustomerForms/SignUpComm';
 import LogInOrg from './components/forms/RootUserForms/LogInOrg';
